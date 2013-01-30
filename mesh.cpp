01ba0d40486bb4218fc8712c8d783894935d8e7a
Serge Ratke <serge.ratke@gmx.de>
2013-01-30 22:24:56 +0100

    graphics: cleanup in mesh code ... fs$§ing rebase.

--- mesh.cpp
+++ mesh.cpp
@@ -13,9 +13,6 @@ Mesh::~Mesh()
 IMesh* Mesh::create(const float* data, const IMesh::UsageHint hint,
 		const VertexFormat format, const int size)
 {
-	BK_ASSERT(data != 0);
-		( (hint == DYNAMIC) ? GL_DYNAMIC_DRAW : GL_STREAM_DRAW );
-
 	MeshImpl* impl = new MeshImpl();
 	impl->init(data, hint, format, size);
 	return impl;
