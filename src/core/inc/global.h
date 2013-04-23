60184d72711a1442015d8bcdb9b39e162a5fd9ab
Serge Ratke <serge.ratke@gmx.de>
2013-04-23 22:14:26 +0200

    core: correct the u64/s64 data types and remove STRINGIFY.

--- src/core/inc/global.h
+++ src/core/inc/global.h
@@ -7,19 +7,19 @@
 
 using namespace std;
 namespace bk {
-	typedef signed   char  s8;
-	typedef unsigned char  u8;
-	typedef signed   short s16;
-	typedef unsigned short u16;
-	typedef signed   int   u32;
-	typedef unsigned int   s32;
-	typedef float          f32;
-	typedef unsigned long  u64;
-	typedef signed   long  s64;
+	typedef signed   char      s8;
+	typedef unsigned char      u8;
+	typedef signed   short     s16;
+	typedef unsigned short     u16;
+	typedef signed   int       u32;
+	typedef unsigned int       s32;
+	typedef unsigned long long u64;
+	typedef signed   long long s64;
+	typedef float              f32;
+	typedef double             f64;
 }
 
 #define BK_IMPL(X) class X##Private* m_impl; friend class X##Private
-#define BK_STRINGIFY(X) #X
 
 #define SAFE_DELETE(X) { if(X) { delete X; X = NULL; } }
 #define SAFE_ARR_DELETE(X) { if(X && X != nullptr) { delete[] X; X = nullptr; }}
