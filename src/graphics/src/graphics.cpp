78a1d5b926c99b1b3dfc48648ef9a9a24d8eae0f
Serge Ratke <serge.ratke@gmx.de>
2013-05-04 14:32:45 +0200

    graphics: add texture to color shader.
    to keep the code simple and not making a distinction as to which (stock)program
    has been chosen -- if at all --, the default variables have to be
    present in order to keep it all working.

--- src/graphics/src/graphics.cpp
+++ src/graphics/src/graphics.cpp
@@ -89,13 +89,16 @@ Graphics::init(const u16 width, const u16 height, const string&)
 		uniform mat4 transformation;
 
 		in vec4 bk_vertex;
+		in vec2 bk_texture0;
 		in vec3 bk_color;
 		in vec3 bk_normal;
 
+		out vec2 bk_fragTex0;
 		out vec3 bk_Color;
 		out vec3 bk_Normal;
 
 		void main() {
+			bk_fragTex0 = bk_texture0;
 			bk_Color = bk_color;
 			bk_Normal = bk_normal;
 			gl_Position = camera * transformation * bk_vertex;
@@ -103,6 +106,7 @@ Graphics::init(const u16 width, const u16 height, const string&)
 	)", R"(
 		#version 130
 
+		in vec2 bk_fragTex0;
 		in vec3 bk_Color;
 		in vec3 bk_Normal;
 
